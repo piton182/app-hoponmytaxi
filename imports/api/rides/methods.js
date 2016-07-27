7b0983059946a904b6279ea6da0ba19981a96ba7
Ivan <ivan.lapshin@39parrot.net>
2016-07-27 19:47:10 +0400

    add methods

--- imports/api/rides/methods.js
+++ imports/api/rides/methods.js
@@ -1,3 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 
 import { Rides } from './rides.js';
+
+
+Meteor.methods(
+	{
+		'rides.update'(ride){
+			if (!this.userId){
+				throw new Meteor.Error('not-authorized');
+			}else{
+				Rides.update({_id: ride._id}, ride);		
+			}
+		}
+	}
+)
